ca0905be3f01e8cd1ac048e449c1dc78deb4efad
morishii <t13949hm@sfc.keio.ac.jp>
2016-12-11 14:35:45 +0900

    Add files via upload

---
+++ Arbieye-test2/Arbieye-test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Arbieye-test2
+//
+//  Created by morishi on 2016/12/10.
+//  Copyright © 2016年 morishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
